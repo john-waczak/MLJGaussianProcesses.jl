bb23bdea303e432821228837d685a126e168bfb2
Documenter.jl <documenter@juliadocs.github.io>
2022-11-09 14:25:21 +0000

    build based on 1de1d0e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MLJGP","category":"page"},{"location":"#MLJGP","page":"Home","title":"MLJGP","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MLJGP.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MLJGP]","category":"page"}]
+}
